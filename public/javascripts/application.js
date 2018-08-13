5ef40637ff4902164f9e88f6c99d5b828ff8007c
Millie Chan <millie.chan@digital.homeoffice.gov.uk>
2018-08-13 11:04:14 +0100

    Updated based on team feedback - changed position (training modules and training course information).

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2626,3 +2626,26 @@ if (
     })
 
 }
+
+// link-back.js
+'use strict';
+
+moj.Modules.backLinks = {
+    link_class: 'link-back',
+
+    init: function() {
+        var self = this;
+
+        self.bindEvents();
+    },
+
+    bindEvents: function() {
+        var self = this;
+
+        $(document).on('click', 'a.' + self.link_class, function(e) {
+            e.preventDefault();
+            window.history.back();
+        });
+    }
+};
+
